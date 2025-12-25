f1bcaaaca2605bc15a9fce8dcf1e0917866c6343
Nilesh <nileshprajapati1108@gmail.com>
2025-12-25 10:26:13 +0530

    fix admin login import path

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Login from "./pages/login.jsx";
+import Login from "./pages/Login.jsx";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useContext } from "react";
